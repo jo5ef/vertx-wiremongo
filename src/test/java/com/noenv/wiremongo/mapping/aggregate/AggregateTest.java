1412ada72f2fbdb634c5fd302d8a3e40c0efa2ef
jo5ef <green-carrot@orange-cactus.org>
2020-11-16 16:02:13 +0100

    refactor tests, remove flowable - part 2
    Signed-off-by: jo5ef <green-carrot@orange-cactus.org>

--- src/test/java/com/noenv/wiremongo/mapping/aggregate/AggregateTest.java
+++ src/test/java/com/noenv/wiremongo/mapping/aggregate/AggregateTest.java
@@ -2,13 +2,11 @@ package com.noenv.wiremongo.mapping.aggregate;
 
 import com.noenv.wiremongo.MemoryStream;
 import com.noenv.wiremongo.TestBase;
-import io.reactivex.Flowable;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
-import io.vertx.reactivex.CompletableHelper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -20,10 +18,10 @@ public class AggregateTest extends TestBase {
     Async async = ctx.async();
     mock.aggregate()
       .inCollection("aggregate")
-      .withPipeline(new JsonArray().add(new JsonObject().put("$match",new JsonObject().put("id","myId"))))
+      .withPipeline(new JsonArray().add(new JsonObject().put("$match", new JsonObject().put("id", "myId"))))
       .returns(MemoryStream.of(new JsonObject().put("x", "y")));
 
-    db.aggregate("aggregate", new JsonArray().add(new JsonObject().put("$match",new JsonObject().put("id","myId"))))
+    db.aggregate("aggregate", new JsonArray().add(new JsonObject().put("$match", new JsonObject().put("id", "myId"))))
       .handler(r -> {
         ctx.assertEquals("y", r.getString("x"));
         async.complete();
@@ -35,10 +33,10 @@ public class AggregateTest extends TestBase {
     Async async = ctx.async();
     mock.aggregate()
       .inCollection("aggregate")
-      .withPipeline(new JsonArray().add(new JsonObject().put("$match",new JsonObject().put("id","myIdError"))))
+      .withPipeline(new JsonArray().add(new JsonObject().put("$match", new JsonObject().put("id", "myIdError"))))
       .returnsError(new Exception("intentional"));
 
-    db.aggregate("aggregate", new JsonArray().add(new JsonObject().put("$match",new JsonObject().put("id","myIdError"))))
+    db.aggregate("aggregate", new JsonArray().add(new JsonObject().put("$match", new JsonObject().put("id", "myIdError"))))
       .exceptionHandler(ex -> {
         ctx.assertEquals("intentional", ex.getMessage());
         async.complete();
@@ -64,66 +62,4 @@ public class AggregateTest extends TestBase {
         async.complete();
       });
   }
-
-//  @Test
-//  @SuppressWarnings("unchecked")
-//  public void testAggregateReturnedObjectNotModified(TestContext ctx) {
-//    final Async async = ctx.async(1 * 2);
-//    final JsonObject given = new JsonObject()
-//      .put("field1", "value1")
-//      .put("field2", "value2")
-//      .put("field3", new JsonObject()
-//        .put("field4", "value3")
-//        .put("field5", "value4")
-//        .put("field6", new JsonArray()
-//          .add("value5")
-//          .add("value6")
-//        )
-//      );
-//    final JsonObject expected = given.copy();
-//
-//    mock.aggregate()
-//      .inCollection("aggregate")
-//      .withPipeline(new JsonArray().add(new JsonObject().put("$match",new JsonObject().put("id","myId"))))
-//      .returns(MemoryStream.of(given));
-//
-//    Flowable.mergeArray(
-//      db.aggregate("aggregate", new JsonArray().add(new JsonObject().put("$match",new JsonObject().put("id","myId")))).toFlowable(),
-//      db.aggregate("aggregate", new JsonArray().add(new JsonObject().put("$match",new JsonObject().put("id","myId")))).toFlowable()
-//    )
-//      .doOnNext(actual -> async.countDown())
-//      .doOnNext(actual -> ctx.assertEquals(expected, actual))
-//      .doOnNext(actual -> {
-//        actual.put("field1", "replace");
-//        actual.remove("field2");
-//        actual.put("add", "add");
-//        actual.getJsonObject("field3").put("field4", "replace");
-//        actual.getJsonObject("field3").remove("field5");
-//        actual.getJsonObject("field3").put("add", "add");
-//        actual.getJsonObject("field3").getJsonArray("field6").remove(0);
-//        actual.getJsonObject("field3").getJsonArray("field6").add("add");
-//      })
-//      .ignoreElements()
-//      .onComplete(ctx.asyncAssertSuccess());
-//  }
-
-//  @Test
-//  @SuppressWarnings("unchecked")
-//  public void testAggregateFileReturnedObjectNotModified(TestContext ctx) {
-//    final Async async = ctx.async(3 * 2);
-//    final JsonObject expected = new JsonObject().put("field1", "value1");
-//
-//    Flowable.mergeArray(
-//      db.aggregate("aggregate", new JsonArray().add(new JsonObject().put("test", "testAggregateFile"))).toFlowable(),
-//      db.aggregate("aggregate", new JsonArray().add(new JsonObject().put("test", "testAggregateFile"))).toFlowable()
-//    )
-//      .doOnNext(actual -> async.countDown())
-//      .doOnNext(actual -> ctx.assertEquals(expected, actual))
-//      .doOnNext(actual -> {
-//        actual.put("field1", "replace");
-//        actual.put("add", "add");
-//      })
-//      .ignoreElements()
-//      .onComplete(ctx.asyncAssertSuccess());
-//  }
 }
